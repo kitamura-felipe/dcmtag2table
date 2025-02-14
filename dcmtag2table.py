84f25a3487c9c27d965c4f081c8155b2a5f92559
Felipe Campos Kitamura <kitamura.felipe@gmail.com>
2025-02-14 07:34:55 -0300

    Update dcmtag2table.py

--- dcmtag2table.py
+++ dcmtag2table.py
@@ -325,7 +325,11 @@ def allow_list(in_path: str, out_path: str, list_of_tags: list, start_pct=1, sta
         # Create a new DICOM dataset
         new_ds = Dataset()
         new_ds.file_meta = FileMetaDataset()
-        new_ds.file_meta.TransferSyntaxUID = original_ds.file_meta.TransferSyntaxUID
+        try:
+            new_ds.file_meta.TransferSyntaxUID = original_ds.file_meta.TransferSyntaxUID
+        except:
+            print("No file_meta.TransferSyntaxUID found. Skipping file.")
+            continue
         
         # Copy only the predefined tags from the original to the new dataset
         for tag in list_of_tags:
